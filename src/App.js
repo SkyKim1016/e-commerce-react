ca901795bcfd8a0089546bc6493f6b5602bca46b
Sky <skykim1016@gmail.com>
2020-06-17 18:56:08 +0900

    74. Header Component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './App.css';
 import HomePage from './pages/hompage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 
+import Header from './components/header/header.component'
+
 // const HatsPage = () => (
 //   <div>
 //     <h1> HATS Page </h1>
@@ -17,6 +19,7 @@ import ShopPage from './pages/shop/shop.component'
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path='/shop' component={ShopPage} /> 
